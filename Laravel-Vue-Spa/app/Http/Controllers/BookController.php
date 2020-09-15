661523c027bfdda5dfb393455eb8fc72f0f94726
kth <thxogusrla@gmail.com>
2020-09-15 21:54:02 +0900

    tdd-spa

---
+++ Laravel-Vue-Spa/app/Http/Controllers/BookController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $redis = Redis::connection();
+        $books = Book::all();
+        Redis::set('books', $books);
+
+        $all_book = $redis->get('books');
+        return $all_book;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function add(Request $request)
+    {
+        Book::create([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+
+        return response()->json("add success");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book=Book::find($id);    
+        $book->update($request->all());
+
+        return response()->json('update success');
+    }
+
+    // edit book
+    public function edit($id)
+    {
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+
+        $book = Book::find($id);
+        $book->delete();
+        return response()->json("delete success");
+    }
+
+}
---
+++ Laravel-Vue-Spa/resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="text-center" style="margin: 20px 0px 20px 0px;">
+            <img src="http://skhu.ac.kr/common/images/kseek/main_banner.jpg"><br>
+            <h3 class="text-secondary">Laravel & Vue CRUD SPA by kth</h3>
+            <a href=""><span class="text-secondary">my Git</span></a>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="/" class="nav-item nav-link">Home</router-link>
+                    <router-link to="/add" class="nav-item nav-link">Add Book</router-link>
+                </div>
+            </div>
+        </nav>
+        <br/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
